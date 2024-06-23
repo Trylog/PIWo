ee303a991fe74446d67fc48172a8bbf8bf108159
Michał Bernacki-Janson <94766391+Trylog@users.noreply.github.com>
2024-06-23 02:37:07 +0200

    Delete lab-react directory

--- lab-react/lab-react/src/data.jsx
+++
@@ -1,7 +0,0 @@
-const startingTodos = [
-    { id: 1, text: "Zrobić pranie" },
-    { id: 2, text: "Kupić samochód" },
-    { id: 3, text: "Zrobić pizzę" },
-  ]
-  
-  export default startingTodos;
